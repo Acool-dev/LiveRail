90c3340efb6b89170bf53a2d9213ea5248ba9c3c
Acool-dev <Lgcoolman97@gmail.com>
2024-11-24 05:17:44 +0300

    Create App.js

---
+++ frontend/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Welcome to LiveRail</h1>
+      <p>Track your trains in real-time.</p>
+    </div>
+  );
+}
+
+export default App;
